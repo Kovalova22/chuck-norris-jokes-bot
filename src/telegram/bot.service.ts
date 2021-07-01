0e6733fced8c3e88b7b0c647181e77ace54cc911
Kovalova22 <ikovalova333@gmail.com>
2021-07-01 23:05:47 +0300

    CHUCKNORRISBOT add greeting msg

--- src/telegram/bot.service.ts
+++ src/telegram/bot.service.ts
@@ -1,6 +1,10 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import * as TelegramBot from 'node-telegram-bot-api';
 
+const token = process.env.TELEGRAM_API_TOKEN;
+
+const bot = new TelegramBot(token, { polling: true });
+
 @Injectable()
 export class BotService implements OnModuleInit {
   onModuleInit() {
@@ -8,20 +12,32 @@ export class BotService implements OnModuleInit {
   }
 
   botMessage() {
-    const token = process.env.TELEGRAM_API_TOKEN;
-
-    const bot = new TelegramBot(token, { polling: true });
-
-    bot.on('message', (msg) => {
-      const Hi = 'hi';
-      if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
-        bot.sendMessage(
-          msg.from.id,
-          'Hello ' +
-            msg.from.first_name +
-            ' what would you like to know about me ?',
-        );
-      }
+    bot.onText(/\/start/, (msg) => {
+      console.log(msg.from.id, msg);
+      bot.sendMessage(
+        msg.from.id,
+        `Hello ${msg.from.first_name}!\nHere you can find Chuck Norris jokes! \nTo get a random joke please press random. \nIf you are interested in categories you can find list of them. \nAnd you are also free to see the last ten jokes you have laughed at!`,
+        {
+          reply_markup: {
+            inline_keyboard: [
+              [
+                {
+                  text: 'Random Joke',
+                  callback_data: 'random',
+                },
+                {
+                  text: 'Categories',
+                  callback_data: 'categories',
+                },
+                {
+                  text: 'History',
+                  callback_data: 'history',
+                },
+              ],
+            ],
+          },
+        },
+      );
     });
   }
 }
