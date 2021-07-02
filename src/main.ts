a7aaaeb2333cb104d5943b7e6d48fe397ef838cb
Kovalova22 <ikovalova333@gmail.com>
2021-07-02 23:57:30 +0300

    add example.env

--- src/main.ts
+++ src/main.ts
@@ -7,5 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
   await app.listen(port);
+
+  console.info(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
