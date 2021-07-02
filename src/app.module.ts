c45c51acd23cae1f668bfe301b6b949d1862c34f
Kovalova22 <ikovalova333@gmail.com>
2021-07-02 14:34:54 +0300

    CHUCKNORRISBOT add db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { JokesService } from './jokesAPI/jokesAPI.service';
 import { BotService } from './telegram/bot.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      database: process.env.DATABASE_NAME,
+      useUnifiedTopology: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, BotService, JokesService],
 })
